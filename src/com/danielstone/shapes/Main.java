6761344a431bf4449547698aa3a90155ae53f2c9
user <user>
2016-03-01 14:15:53 +0000

    Initial commit

---
+++ src/com/danielstone/shapes/Main.java
@@ -0,0 +1,24 @@
+package com.danielstone.shapes;
+
+import javafx.application.Application;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    //Stage variable. Use this to control the stage.
+    Stage window;
+    // Graphics context, used to
+    GraphicsContext gc;
+
+
+    public static void main(String[] args) {
+	launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
